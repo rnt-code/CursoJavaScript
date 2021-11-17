f034c79e941497c00ebba25797cc005e8ddaa641
Ramón Taboada <rntaboada@yahoo.com.ar>
2021-11-16 22:42:02 -0300

    otro commit

---
+++ Leccion03/03-01-OperadoresAritméticos.js
@@ -0,0 +1,20 @@
+let a = 3;
+let b = 2;
+let z;
+//suma
+z = a + b;
+console.log('la suma es: '+z)
+//resta
+z = a - b;
+console.log('la resta es: '+z);
+//multiplicación
+z = a * b;
+console.log('la multiplicación es: '+z);
+//división
+z = a / b;
+console.log('la división es: '+z);
+
+//exponenciación
+z = a ** b;
+console.log('exponenciación "a" elevado a la "b" es: '+z);
+
