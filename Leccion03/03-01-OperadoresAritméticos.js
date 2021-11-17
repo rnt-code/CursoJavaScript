90d402f107518f983b7da50e2091f9582c3a757f
Ramón Taboada <rntaboada@yahoo.com.ar>
2021-11-17 15:56:09 -0300

    otro..

--- Leccion03/03-01-OperadoresAritméticos.js
+++ Leccion03/03-01-OperadoresAritméticos.js
@@ -1,6 +1,7 @@
 let a = 3;
 let b = 2;
 let z;
+let r;
 //suma
 z = a + b;
 console.log('la suma es: '+z)
@@ -13,8 +14,9 @@ console.log('la multiplicación es: '+z);
 //división
 z = a / b;
 console.log('la división es: '+z);
-
+//residuo o resto de la división
+r = a % b;
+console.log('el resto de la división es: '+r);
 //exponenciación
 z = a ** b;
 console.log('exponenciación "a" elevado a la "b" es: '+z);
-
---
+++ Leccion03/03-02IncrementoDecremento.js
@@ -0,0 +1,14 @@
+//Incremento: ++
+//Decremento: --
+let a = 3;
+let b = 2;
+let z = 0;
+console.log(z);
+
+//Pre-incremento (el operador ++ por delante de la variable)
+z = ++a; //primero se incrementa (pasa de 'a' a 'a+1'), y luego se asigna.
+console.log(z);
+
+//Post-incremento (el operador ++ después de la variable)
+z = a++; //
+console.log(z);
