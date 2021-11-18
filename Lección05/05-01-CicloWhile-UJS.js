036fc891afef5e20ac3e6721310d38b8282b6d01
Ramón Taboada <rntaboada@yahoo.com.ar>
2021-11-18 15:01:04 -0300

    fin arreglos...

---
+++ Lección05/05-01-CicloWhile-UJS.js
@@ -0,0 +1,8 @@
+let contador = 0;
+
+//puede no ejecutarse ni una vez
+while( contador < 3 ){
+    console.log(contador);
+    contador++;
+}
+console.log("Fin ciclo while");
---
+++ Lección06/06-01-ArreglosJavaScript-UJS.js
@@ -0,0 +1,14 @@
+//Arreglos, es un tipo object.
+
+//formas de declarar arreglos
+//sitaxis clásica:
+//let autos = new Array('BMW','Mercedes Benz','Volvo');
+/*
+let autos;
+autos = new Array('BMW','Mercedes Benz','Volvo');
+*/
+
+//sintaxix más moderna:
+const autos = ['BMW','Mercedes Benz','Volvo'];
+
+console.log(autos);
