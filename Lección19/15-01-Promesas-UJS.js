e0ab2221d883d767f6af294f100648ce08428b3f
Ramón Taboada <rntaboada@yahoo.com.ar>
2021-11-30 00:29:07 -0300

    reforzando asincronía...

--- Lección19/15-01-Promesas-UJS.js
+++ Lección19/15-01-Promesas-UJS.js
@@ -1,10 +1,14 @@
-let miPromesa = new Promise((resolver, rechazar) => {
-    let expresion = true;
-    if(expresion)
-        resolver('Resolvió correcto');
-    else
-        rechazar('Se produjo un error');    
-});
+let miPromesa = new Promise(
+    
+    (resolver, rechazar) => {
+
+        let expresion = true;
+        if(expresion)
+            resolver('Resolvió correcto');
+        else
+            rechazar('Se produjo un error');    
+    }
+);
 
 //miPromesa.then( valor => console.log(valor), error => console.log(error));
 miPromesa.then(valor => console.log(valor)).catch(error=>console.log(error));
