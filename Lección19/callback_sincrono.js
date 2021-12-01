21c77b93336c4ab67c9147014bae53747d4974ef
Ramón Taboada <rntaboada@yahoo.com.ar>
2021-11-30 23:43:27 -0300

    otra..

--- Lección19/callback_sincrono.js
+++ Lección19/callback_sincrono.js
@@ -1,14 +1,27 @@
-function saludar(nombre) {
-    console.log('Hola ' + nombre);
-  }
+// https://www.youtube.com/watch?v=oVeqw1bLOZE
+
+//Un callback es una función que se ejecuta a través de otra función
+//Los callbacks no son asíncronos
+
+  //cd = callback
+
+const getUser = (id, cb) => {
+
+    const user = 
+    {
+        name: 'Dorian',
+        id:id
+    }
   
-  function procesarEntradaUsuario(callback) {
-    var nombre = console.log('Por favor ingresa tu nombre.');
-    callback(nombre);
-  }
-  
-  procesarEntradaUsuario(saludar('Ramon')); //saluda es una función
+    cb(null,user);    
+}
+
+getUser(1, (err, user) => {
+    
+    if(err) return console.log(err)
+    console.log(`User name is ${user.name}`)
+});
 
 
 
-  https://www.youtube.com/watch?v=oVeqw1bLOZE
+//https://www.youtube.com/watch?v=gMqF-D3kuLE
