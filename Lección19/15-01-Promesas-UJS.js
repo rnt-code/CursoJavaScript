18961709ebcd58c4dcf2b0a6fda39ed1238874bd
Ramón Taboada <rntaboada@yahoo.com.ar>
2021-12-09 15:45:45 -0300

    otro..

--- Lección19/15-01-Promesas-UJS.js
+++ Lección19/15-01-Promesas-UJS.js
@@ -1,14 +1,20 @@
-let miPromesa = new Promise(
-    
-    (resolver, rechazar) => {
+let miPromesa = new Promise((resolver, rechazar) => {
 
-        let expresion = true;
-        if(expresion)
-            resolver('Resolvió correcto');
-        else
-            rechazar('Se produjo un error');    
-    }
-);
+    let expresion = true;
+    if(expresion)
+        resolver('Resolvió correctamente');
+    else
+        rechazar('Se produjo un error');    
+
+});
 
 //miPromesa.then( valor => console.log(valor), error => console.log(error));
-miPromesa.then(valor => console.log(valor)).catch(error=>console.log(error));
+miPromesa
+    .then(valor => console.log(valor))
+    .catch(error=>console.error(error));
+
+//la promesa puede tener solo uno de los parámetros
+
+let promesa = new Promise((resolver) => {
+    
+});
